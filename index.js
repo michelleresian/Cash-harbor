b7e51d4d6e537784c331e5887bf7914ab92a713a
Michelle Resian <you@example.com>
2024-01-20 00:01:46 +0300

    index.html

---
+++ index.js
@@ -0,0 +1,5 @@
+const accessKey = "Jdt7KLPS0eERHiVnRrpcq0m5Tdd7DCwcdieUTu-XCNA"
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search.input")
+const search
